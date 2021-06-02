97e05ef8369de01f2a4ce04e64b9e22142887081
Andy Koch <akandrewkoch@gmail.com>
2021-06-02 09:54:22 -0500

    -adding form, through photo input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,6 +10,13 @@ class EditProductScreen extends StatefulWidget {
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _urlController = TextEditingController();
+
+  void dispose() {
+    _urlController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     Product? editingProduct;
@@ -19,14 +26,77 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(editingProduct!=null? "Editing ${editingProduct.title}" : "New Product"),
+        title: Text(editingProduct != null
+            ? "Editing ${editingProduct.title}"
+            : "New Product"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
           child: ListView(
             children: [
-              TextFormField(decoration: InputDecoration(labelText: "Title"), textInputAction: TextInputAction.next, initialValue: editingProduct!=null?editingProduct.title:null,),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Title"),
+                textInputAction: TextInputAction.next,
+                initialValue:
+                    editingProduct != null ? editingProduct.title : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Price"),
+                keyboardType: TextInputType.number,
+                textInputAction: TextInputAction.next,
+                initialValue: editingProduct != null
+                    ? editingProduct.price.toStringAsFixed(2)
+                    : null,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Description"),
+                maxLines: 3,
+                textInputAction: TextInputAction.next,
+                initialValue:
+                    editingProduct != null ? editingProduct.description : null,
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _urlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _urlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Image URL",
+                      ),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _urlController,
+                      onEditingComplete: () {
+                        setState(() {
+
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
